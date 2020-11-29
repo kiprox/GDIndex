fbc248962cef7ebe0869d730e4d3a9557f5bca34
kiprox <70131116+kiprox@users.noreply.github.com>
2020-11-29 23:33:02 +0700

    initial release

---
+++ web/src/locales/en.js
@@ -0,0 +1,15 @@
+export default {
+	fileName: 'File Name',
+	modifiedTime: 'Modified Time',
+	fileSize: 'File Size',
+	mainDrive: 'Main Drive',
+	search: 'Search',
+	fileUpload: 'File Upload',
+	urlUpload: 'Upload from Url',
+	upload: 'Upload',
+	fileToUpload: 'File to upload',
+	uploading: 'Uploading...',
+	serverProcessing: 'Server is processing the file now',
+	bigFileUploadWarning:
+		"Limitation, uploading bigfiles may randomly failed.",
+}
